4eacecf3b4f2c9fa2fa90c9b0fa5b2b17e717ced
Mohamed Saleh <msaleh.eg87@gmail.com>
2018-11-16 02:13:22 +0200

    feat(service): the fastest hello world in the whole world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+extern crate hyper;
+
+use hyper::{Body, Request, Response, Server};
+use hyper::rt::Future;
+use hyper::service::service_fn_ok;
+
 fn main() {
+    const PHRASE: &str = "Hello, World!";
+
+    fn hello_world(_req: Request<Body>) -> Response<Body> {
+        Response::new(Body::from(PHRASE))
+    }
+
+    let addr = ([127, 0, 0, 1], 3000).into();
+
+    let new_svc = || {
+        service_fn_ok(hello_world)
+    };
+
+    let server = Server::bind(&addr)
+        .serve(new_svc)
+        .map_err(|e| eprintln!("server error: {}", e));
+
+    hyper::rt::run(server);
+
     println!("Hello, world!");
 }
